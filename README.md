1c9821666a7403a7b2277fd5b104b94ce4ba4b66
nezox <mpolakov777@gmail.com>
2025-06-14 23:12:37 +0300

    added prettier and the files configured .prettier/eslint.config

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Stack:
 - TypeScript
 - SCSS
 - Redux Toolkit
+- SWR
 - React Hook Form
 - Zod
-
 
 Author of the web design - https://www.figma.com/@slothui
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ["@phosphor-icons/react"],
-  },
-};
+	experimental: {
+		optimizePackageImports: ['@phosphor-icons/react'],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
